c409b69ce130637973a9b84f31643f407415b89f
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-02-12 14:32:12 -0300

    Register expense with division of payents

--- src/main/java/com/jota/splitsmart/controller/ExpenseController.java
+++ src/main/java/com/jota/splitsmart/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpenseController {
 
     private final ExpenseService expenseService;
+    private static final String EXPENSE_PAYER_ID = "x-expense-payer-id";
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ExpenseDTO register(@RequestBody final ExpenseDTO expenseDTO) {
-        return expenseService.register(expenseDTO);
+    public ExpenseDTO register(@RequestHeader(name = EXPENSE_PAYER_ID) final Long userId,
+        @RequestBody final ExpenseDTO expenseDTO) {
+        return expenseService.register(userId, expenseDTO);
     }
 }
--- src/main/java/com/jota/splitsmart/mapper/UserPaysExpenseMapper.java
+++ src/main/java/com/jota/splitsmart/mapper/UserPaysExpenseMapper.java
@@ -1,21 +1,21 @@
 package com.jota.splitsmart.mapper;
 
-import com.jota.splitsmart.persistence.model.Expense;
-import com.jota.splitsmart.service.expenseservice.dto.ExpenseDTO;
+import com.jota.splitsmart.persistence.model.UserPaysExpense;
+import java.math.BigDecimal;
 import java.time.Instant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", imports = Instant.class)
-public abstract class ExpenseMapper {
+public abstract class UserPaysExpenseMapper {
 
     @Mappings({
         @Mapping(expression = "java(Instant.now())", target = "createdAt"),
         @Mapping(expression = "java(Instant.now())", target = "updatedAt"),
-        @Mapping(source = "userId", target = "userId")
+        @Mapping(constant = "false", target = "isPayed"),
+        // @Mapping(source = "userId", target = "userId")
     })
-    public abstract Expense mapToExpense(final ExpenseDTO expenseDTO, final Long userId);
-
-    public abstract ExpenseDTO mapToRegisterExpenseResponse(final Expense expense);
+    public abstract UserPaysExpense mapToUserPaysExpense(final Long userId, final Long expenseId,
+        final BigDecimal amount);
 }
