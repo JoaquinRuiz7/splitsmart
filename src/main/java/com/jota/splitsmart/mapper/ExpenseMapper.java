e3a49c6afe6f4d73bfc476f26ceb17daf5fd765a
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-02-11 22:13:36 -0300

    Fixed mappers

--- src/main/java/com/jota/splitsmart/mapper/ExpenseMapper.java
+++ src/main/java/com/jota/splitsmart/mapper/ExpenseMapper.java
@@ -4,10 +4,16 @@ import com.jota.splitsmart.persistence.model.Expense;
 import com.jota.splitsmart.service.expenseservice.dto.ExpenseDTO;
 import java.time.Instant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", imports = Instant.class)
 public abstract class ExpenseMapper {
 
+    @Mappings({
+        @Mapping(expression = "java(Instant.now())", target = "createdAt"),
+        @Mapping(expression = "java(Instant.now())", target = "updatedAt"),
+    })
     public abstract Expense mapToExpense(final ExpenseDTO expenseDTO);
 
     public abstract ExpenseDTO mapToRegisterExpenseResponse(final Expense expense);
--- src/main/java/com/jota/splitsmart/service/userservice/UserService.java
+++ src/main/java/com/jota/splitsmart/service/userservice/UserService.java
@@ -4,7 +4,6 @@ import com.jota.splitsmart.mapper.UserMapper;
 import com.jota.splitsmart.persistence.model.User;
 import com.jota.splitsmart.persistence.repository.UserRepository;
 import com.jota.splitsmart.service.userservice.dto.UserDTO;
-import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +17,6 @@ public class UserService {
 
     public UserDTO register(final UserDTO userDTO) {
         final User user = userMapper.mapToUser(userDTO);
-        user.setCreatedAt(Instant.now());
-        user.setUpdatedAt(Instant.now());
         userRepository.save(user);
         return userMapper.mapToUserDTO(user);
     }
