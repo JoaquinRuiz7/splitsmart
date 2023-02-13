60a902856d7a71775e1407d9a0a8a9888a783541
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-02-12 21:28:38 -0300

    Validate user already registered

--- src/main/java/com/jota/splitsmart/controller/ExceptionHandlerController.java
+++ src/main/java/com/jota/splitsmart/controller/ExceptionHandlerController.java
@@ -2,8 +2,10 @@ package com.jota.splitsmart.controller;
 
 import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import com.jota.splitsmart.exception.UserAlreadyRegisteredException;
 import com.jota.splitsmart.exception.UserNotFoundException;
 import com.jota.splitsmart.exchangedata.ErrorDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +35,16 @@ public class ExceptionHandlerController {
         return ErrorDTO.builder()
             .message(userNotFoundException.getMessage())
             .status(NOT_FOUND.value())
+            .build();
+    }
+
+    @ExceptionHandler(UserAlreadyRegisteredException.class)
+    @ResponseStatus(CONFLICT)
+    public ErrorDTO userNotFound(final UserAlreadyRegisteredException userAlreadyRegisteredException) {
+        log.error(userAlreadyRegisteredException.getMessage());
+        return ErrorDTO.builder()
+            .message(userAlreadyRegisteredException.getMessage())
+            .status(CONFLICT.value())
             .build();
     }
 
