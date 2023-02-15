7536c77018d022588158cab5f52d667ca10c3d0e
JoaquinRuiz7 <47424155+JoaquinRuiz7@users.noreply.github.com>
2023-02-15 19:30:49 -0300

    Added header expense payer id to context data (#8)
    Co-authored-by: Joaquin Ruiz <jruiz@nocnocstore.com>

--- src/main/java/com/jota/splitsmart/context/ContextData.java
+++ src/main/java/com/jota/splitsmart/context/ContextData.java
@@ -4,4 +4,5 @@ public abstract class ContextData {
 
     public static final String BASE_PACKAGE = "com.jota.splitsmart";
     public static final String REPOSITORIES_PACKAGE = "com.jota.splitsmart.persistence.repository";
+    public static final String EXPENSE_PAYER_ID = "X-Expense-Payer-Id";
 }
