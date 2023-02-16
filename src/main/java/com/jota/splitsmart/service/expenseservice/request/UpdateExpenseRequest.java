ee8859241b5ff1a7bd140277395a91ca8dd87cff
JoaquinRuiz7 <47424155+JoaquinRuiz7@users.noreply.github.com>
2023-02-16 18:58:32 -0300

    Update expense (#10)
    * Update expense
    
    * Remove un sued code
    
    ---------
    
    Co-authored-by: Joaquin Ruiz <jruiz@nocnocstore.com>

---
+++ src/main/java/com/jota/splitsmart/service/expenseservice/request/UpdateExpenseRequest.java
@@ -0,0 +1,20 @@
+package com.jota.splitsmart.service.expenseservice.request;
+
+import java.math.BigDecimal;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class UpdateExpenseRequest {
+
+    @NotEmpty
+    @NotNull
+    private String description;
+
+    @NotEmpty
+    @NotNull
+    @Positive
+    private BigDecimal total;
+}
