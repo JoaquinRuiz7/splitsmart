13c1ca3f7169ecaa300fd56d2d1354ac6d24014f
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-02-23 14:37:32 -0300

    Security config for registration

--- src/main/java/com/jota/splitsmart/service/userservice/UserService.java
+++ src/main/java/com/jota/splitsmart/service/userservice/UserService.java
@@ -6,8 +6,10 @@ import com.jota.splitsmart.exception.UserAlreadyRegisteredException;
 import com.jota.splitsmart.mapper.UserMapper;
 import com.jota.splitsmart.persistence.model.User;
 import com.jota.splitsmart.persistence.repository.UserRepository;
-import com.jota.splitsmart.service.userservice.dto.UserDTO;
+import com.jota.splitsmart.service.userservice.request.RegisterUserRequest;
+import com.jota.splitsmart.service.userservice.response.RegisterUserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,15 +18,16 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-
-    public UserDTO register(final UserDTO userDTO) {
+    public RegisterUserResponse register(final RegisterUserRequest userDTO) {
 
         if (userRepository.existsByEmail(userDTO.getEmail())) {
             throw new UserAlreadyRegisteredException(format("User with email %s already exists.", userDTO.getEmail()));
         }
 
         final User user = userMapper.mapToUser(userDTO);
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         userRepository.save(user);
         return userMapper.mapToUserDTO(user);
     }
