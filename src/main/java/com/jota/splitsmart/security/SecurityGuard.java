328b1fd577417491ded9fbc4cb4a3e276ae54c9e
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-02-27 16:26:18 -0300

    secured endpoints

--- src/main/java/com/jota/splitsmart/security/SecurityGuard.java
+++ src/main/java/com/jota/splitsmart/security/SecurityGuard.java
@@ -3,7 +3,9 @@ package com.jota.splitsmart.security;
 import com.jota.splitsmart.exception.ExpenseNotFoundException;
 import com.jota.splitsmart.exception.ForbiddenException;
 import com.jota.splitsmart.persistence.model.Expense;
+import com.jota.splitsmart.persistence.model.User;
 import com.jota.splitsmart.persistence.repository.ExpenseRepository;
+import com.jota.splitsmart.persistence.repository.FriendRequestRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,7 @@ public class SecurityGuard {
     private final JWT jwt;
     private final HttpServletRequest httpServletRequest;
     private final ExpenseRepository expenseRepository;
+    private final FriendRequestRepository friendRequestRepository;
 
     private static final String FORBIDDEN = "Forbidden";
 
@@ -40,12 +43,24 @@ public class SecurityGuard {
     }
 
     private String getTokenFromRequest(final HttpServletRequest request) {
-        String headerAuth = request.getHeader("Authorization");
+        final String headerAuth = request.getHeader("Authorization");
 
         if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7);
         }
 
         return null;
+    }
+
+    public void checkIfFriendRequestBelongsToUser(final Long friendRequestId) {
+        final String token = getTokenFromRequest(httpServletRequest);
+        final Long tokenId = jwt.getUserIdFromToken(token);
+
+        friendRequestRepository.findById(friendRequestId).ifPresent(friendRequest -> {
+            User user = friendRequest.getUser();
+            if (!user.getId().equals(tokenId)) {
+                throw new ForbiddenException(FORBIDDEN);
+            }
+        });
     }
 }
