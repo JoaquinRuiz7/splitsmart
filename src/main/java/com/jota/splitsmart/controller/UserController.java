f221252994715f00cf22d322c03b6045781934f5
Joaquin Ruiz <jruiz@nocnocstore.com>
2023-03-11 19:15:17 -0300

    user update info

--- src/main/java/com/jota/splitsmart/controller/UserController.java
+++ src/main/java/com/jota/splitsmart/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.jota.splitsmart.controller;
 
-import com.jota.splitsmart.service.UserService;
 import com.jota.splitsmart.exchangedata.user.RegisterUserRequest;
 import com.jota.splitsmart.exchangedata.user.RegisterUserResponse;
+import com.jota.splitsmart.service.UserService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +23,13 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public RegisterUserResponse register(@RequestBody final RegisterUserRequest userDTO) {
-        return userService.register(userDTO);
+    public RegisterUserResponse register(@RequestBody @Valid final RegisterUserRequest request) {
+        return userService.register(request);
+    }
+
+    @PatchMapping("/{userId}")
+    public RegisterUserResponse update(@PathVariable final Long userId,
+        @RequestBody @Valid final RegisterUserRequest request) {
+        return userService.update(userId, request);
     }
 }
--- src/main/java/com/jota/splitsmart/exchangedata/user/RegisterUserRequest.java
+++ src/main/java/com/jota/splitsmart/exchangedata/user/RegisterUserRequest.java
@@ -1,13 +1,17 @@
 package com.jota.splitsmart.exchangedata.user;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
 
-    private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String email;
+    @NotEmpty
     private String cellphone;
+    @NotEmpty
     private String password;
 }
