50e5cdfde4072b48d9fae2f8c2e02726728af38e
JoaquinRuiz7 <47424155+JoaquinRuiz7@users.noreply.github.com>
2023-03-11 19:17:42 -0300

    Update user (#24)
    * user update info
    
    * security guard
    
    ---------
    
    Co-authored-by: Joaquin Ruiz <jruiz@nocnocstore.com>

--- src/main/java/com/jota/splitsmart/controller/UserController.java
+++ src/main/java/com/jota/splitsmart/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.jota.splitsmart.controller;
 
-import com.jota.splitsmart.service.UserService;
 import com.jota.splitsmart.exchangedata.user.RegisterUserRequest;
 import com.jota.splitsmart.exchangedata.user.RegisterUserResponse;
+import com.jota.splitsmart.security.SecurityGuard;
+import com.jota.splitsmart.service.UserService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final SecurityGuard securityGuard;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public RegisterUserResponse register(@RequestBody final RegisterUserRequest userDTO) {
-        return userService.register(userDTO);
+    public RegisterUserResponse register(@RequestBody @Valid final RegisterUserRequest request) {
+        return userService.register(request);
+    }
+
+    @PatchMapping("/{userId}")
+    public RegisterUserResponse update(@PathVariable final Long userId,
+        @RequestBody @Valid final RegisterUserRequest request) {
+        securityGuard.checkIfTokenBelongsToUser(userId);
+        return userService.update(userId, request);
     }
 }
--- src/main/java/com/jota/splitsmart/service/UserService.java
+++ src/main/java/com/jota/splitsmart/service/UserService.java
@@ -3,11 +3,13 @@ package com.jota.splitsmart.service;
 import static java.lang.String.format;
 
 import com.jota.splitsmart.exception.UserAlreadyRegisteredException;
+import com.jota.splitsmart.exception.UserNotFoundException;
+import com.jota.splitsmart.exchangedata.user.RegisterUserRequest;
+import com.jota.splitsmart.exchangedata.user.RegisterUserResponse;
 import com.jota.splitsmart.mapper.UserMapper;
 import com.jota.splitsmart.persistence.model.User;
 import com.jota.splitsmart.persistence.repository.UserRepository;
-import com.jota.splitsmart.exchangedata.user.RegisterUserRequest;
-import com.jota.splitsmart.exchangedata.user.RegisterUserResponse;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,15 +22,40 @@ public class UserService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
-    public RegisterUserResponse register(final RegisterUserRequest userDTO) {
-
-        if (userRepository.existsByEmail(userDTO.getEmail())) {
-            throw new UserAlreadyRegisteredException(format("User with email %s already exists.", userDTO.getEmail()));
-        }
-
-        final User user = userMapper.mapToUser(userDTO);
-        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+    public RegisterUserResponse register(final RegisterUserRequest request) {
+        checkIfMailIsAssignable(request);
+        final User user = userMapper.mapToUser(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         userRepository.save(user);
         return userMapper.mapToUserDTO(user);
     }
+
+    public RegisterUserResponse update(final Long userId, final RegisterUserRequest request) {
+        checkIfMailIsAssignable(request);
+        User user = userRepository.findById(userId).orElseThrow(() -> {
+            throw new UserNotFoundException(format("User with id %s not found", userId));
+        });
+        user = setUserInfo(user, request);
+        userRepository.save(user);
+
+        return userMapper.mapToUserDTO(user);
+    }
+
+    private User setUserInfo(final User user, final RegisterUserRequest request) {
+        user.setEmail(request.getEmail() != null ? request.getEmail() : user.getEmail());
+        user.setPassword(
+            request.getPassword() != null ? passwordEncoder.encode(request.getPassword()) : user.getPassword());
+        user.setCellphone(request.getCellphone() != null ? request.getCellphone() : user.getCellphone());
+        user.setName(request.getName() != null ? request.getName() : user.getName());
+        user.setUpdatedAt(Instant.now());
+        return user;
+    }
+
+    private void checkIfMailIsAssignable(RegisterUserRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new UserAlreadyRegisteredException(format("User with email %s already exists.", request.getEmail()));
+        }
+    }
+
+
 }
