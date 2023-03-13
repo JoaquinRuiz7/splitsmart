37e6d042179ed5e65e5f3e930a2e18079b0b9bde
JoaquinRuiz7 <47424155+JoaquinRuiz7@users.noreply.github.com>
2023-03-12 23:22:31 -0300

    remove friend endpoint (#25)
    Co-authored-by: Joaquin Ruiz <jruiz@nocnocstore.com>

--- src/main/java/com/jota/splitsmart/service/UserFriendService.java
+++ src/main/java/com/jota/splitsmart/service/UserFriendService.java
@@ -1,12 +1,16 @@
 package com.jota.splitsmart.service;
 
+import com.jota.splitsmart.exception.FriendAlreadyAddedException;
+import com.jota.splitsmart.exchangedata.userfriend.FriendResponseDTO;
 import com.jota.splitsmart.mapper.UserFriendMapper;
 import com.jota.splitsmart.persistence.repository.UserFriendRepository;
-import com.jota.splitsmart.exchangedata.userfriend.FriendResponseDTO;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserFriendService {
@@ -16,5 +20,14 @@ public class UserFriendService {
 
     public List<FriendResponseDTO> getUserFriends(final Long userId) {
         return userFriendRepository.findAllByUserId(userId).stream().map(userFriendMapper::mapToUserFriendDTO).toList();
+    }
+
+    @Transactional(transactionManager = "splitSmartTransactionManager")
+    public void deleteFriend(final Long userId, final Long friendId) {
+        if (!userFriendRepository.existsByUserIdAndFriendId(userId, friendId)) {
+            throw new FriendAlreadyAddedException("Contact already unfriend or not on your friend list ");
+        }
+        userFriendRepository.deleteFriend(friendId);
+        log.info("Deleted friend with id {}", friendId);
     }
 }
